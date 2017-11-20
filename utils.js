3fbad9737c4ab096c5d0059e7577e5dafd8f05f6
AkimotoAkari <10473457+AkimotoAkari@users.noreply.github.com>
2017-11-20 18:40:21 +0800

    Added Full-Width Number Support

--- utils.js
+++ utils.js
@@ -4,9 +4,12 @@ let py = require('pinyin'),
 
 let chineseCharacterRegex = /^[\u4E00-\u9FCC\u3400-\u4DB5\uFA0E\uFA0F\uFA11\uFA13\uFA14\uFA1F\uFA21\uFA23\uFA24\uFA27-\uFA29]|[\ud840-\ud868][\udc00-\udfff]|\ud869[\udc00-\uded6\udf00-\udfff]|[\ud86a-\ud86c][\udc00-\udfff]|\ud86d[\udc00-\udf34\udf40-\udfff]|\ud86e[\udc00-\udc1d]$/;
 
+let fullWidthNumRegex = /^[\uFF10-\uFF19]$/;
+
 function typeOfChar( chr ) {
     if( /[a-zA-Z]/.test( chr ) ) return 2;
     if( /[0-9]/.test( chr ) ) return 3;
+    if( fullWidthNumRegex.test( chr ) ) return 4;
     if( chineseCharacterRegex.test(chr) ) return 1001;
     if( XRegExp('^[\\p{Hiragana}]$').test( chr ) ) return 1002;
     if( XRegExp('^[\\p{Katakana}]$').test( chr ) ) return 1003;
@@ -27,9 +30,16 @@ function _toPinyin( str ) {
         .join('-');
 }
 
+function _toHalfWidth( str ) {
+    return str.replace(/[\uFF10-\uFF19]/g, (m) => 
+        String.fromCharCode(m.charCodeAt(0) - 0xfee0)
+    );
+}
+
 function processor( [type, str] ) {
     if( type === 1001 ) return _toPinyin( str );
     if( type === 1002 | type === 1003 ) return wk.toRomaji( str );
+    if( type === 4 ) return _toHalfWidth( str );
     return str;
 }
 
