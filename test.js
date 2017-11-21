d2f797984c105db030e5503ad32652ef11e9b6cc
AkimotoAkari <10473457+AkimotoAkari@users.noreply.github.com>
2017-11-21 18:06:51 +0800

    Some minor fixes

--- test.js
+++ test.js
@@ -1,10 +1,13 @@
-let { genearateUid:g } = require('.');
+let { generateUid:g } = require('.');
 
 let tests = [
     //[string, description]
-    ['阿暖あにクロイ-122UUU&&&^-……「」', 'default test']
+    ['阿暖あにクロイ-122UUU&&&^-……「」', 'default test'],
+    ['冰封1000アイス１０００', 'full-width numbers'],
+    ['ABC・abc', 'full-width punctuations'],
+    ['aa---bb', 'multiple dashes'],
 ];
 
 for( let item of tests ) {
-    console.log(`Result for \x1b[33m${item[0]}\x1b[0m${item[1]?'('+item[1]+')':''}: \x1b[34m${g(item[0])}\x1b[0m`);
+    console.log(`Result for \x1b[33m${item[0]}\x1b[0m${item[1]?' ('+item[1]+')':''}: \x1b[34m${g(item[0])}\x1b[0m`);
 }
--- utils.js
+++ utils.js
@@ -1,18 +1,16 @@
 let py = require('pinyin'),
     XRegExp = require('xregexp'),
-    wk = require('wanakana');
-
-let chineseCharacterRegex = /^[\u4E00-\u9FCC\u3400-\u4DB5\uFA0E\uFA0F\uFA11\uFA13\uFA14\uFA1F\uFA21\uFA23\uFA24\uFA27-\uFA29]|[\ud840-\ud868][\udc00-\udfff]|\ud869[\udc00-\uded6\udf00-\udfff]|[\ud86a-\ud86c][\udc00-\udfff]|\ud86d[\udc00-\udf34\udf40-\udfff]|\ud86e[\udc00-\udc1d]$/;
-
-let fullWidthNumRegex = /^[\uFF10-\uFF19]$/;
+    wk = require('wanakana'),
+    regexps = require('./regexps.js');
 
 function typeOfChar( chr ) {
     if( /[a-zA-Z]/.test( chr ) ) return 2;
     if( /[0-9]/.test( chr ) ) return 3;
-    if( fullWidthNumRegex.test( chr ) ) return 4;
-    if( chineseCharacterRegex.test(chr) ) return 1001;
-    if( XRegExp('^[\\p{Hiragana}]$').test( chr ) ) return 1002;
-    if( XRegExp('^[\\p{Katakana}]$').test( chr ) ) return 1003;
+    if( regexps.fwn.test( chr ) ) return 4;
+    if( regexps.p.test( chr ) ) return 5;
+    if( regexps.cc.test( chr ) ) return 1001;
+    if( regexps.hrgn.test( chr ) ) return 1002;
+    if( regexps.ktkn.test( chr ) ) return 1003;
     if( chr === '-' ) return 1;
     return 0;
 }
@@ -40,6 +38,7 @@ function processor( [type, str] ) {
     if( type === 1001 ) return _toPinyin( str );
     if( type === 1002 | type === 1003 ) return wk.toRomaji( str );
     if( type === 4 ) return _toHalfWidth( str );
+    if( type === 5 ) return '-';
     return str;
 }
 
